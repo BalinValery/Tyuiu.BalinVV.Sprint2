2294716717d62c749a369470f13c8b0e2db7619e
BalinValery <valera.balin00@mail.ru>
2025-11-12 00:58:33 +0500

    Task6

---
+++ Tyuiu.BalinVV.Sprint2.Task6.V10.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.BalinVV.Sprint2.Task6.V10.Lib;
+namespace Tyuiu.BalinVV.Sprint2.Task6.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckFindDateOfPreviousDay()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual("10.11.2025", ds.FindDateOfPreviousDay(2025, 11, 11));
+        }
+    }
+}
---
+++ Tyuiu.BalinVV.Sprint2.Task6.V10/Program.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.BalinVV.Sprint2.Task6.V10.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите номер дня: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите номер месяца: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите номер года: ");
+        int g = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Дата предыдущего дня: " + ds.FindDateOfPreviousDay(g, m, n));
+
+        Console.ReadKey();
+    }
+}
