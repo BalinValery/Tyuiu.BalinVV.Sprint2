92284fb925df9f1ca308c9056aeaa60597d2af98
BalinValery <valera.balin00@mail.ru>
2025-11-11 19:33:15 +0500

    Task3

---
+++ Tyuiu.BalinVV.Sprint2.Task3.V13.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.BalinVV.Sprint2.Task3.V13.Lib;
+namespace Tyuiu.BalinVV.Sprint2.Task3.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(0.75, ds.Calculate(0));
+        }
+    }
+}
---
+++ Tyuiu.BalinVV.Sprint2.Task3.V13/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.BalinVV.Sprint2.Task3.V13.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #2 | Выполнил: Балин В.В. | СМАРТб-25-1";
+        
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Вложенные операторы if - else                                           *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #13                                                             *");
+        Console.WriteLine("* Выполнил: Балин В.В. | СМАРТб-25-1                                      *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+        Console.WriteLine("*Написать программу, которая вычисляет требуемое значение функции Y с испо*");
+        Console.WriteLine("*ользованием вложенных оператор if-else, где пользователь вводит значение *");
+        Console.WriteLine("* переменной X с клавиатуры. Округлить полученное значение до трех знаков *");
+        Console.WriteLine("* после запятой;                                                          *");
+
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Введите x:");
+        var x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("***************************************************************************");
+        
+
+        
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        
+        
+        var result = ds.Calculate(x);
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+}
