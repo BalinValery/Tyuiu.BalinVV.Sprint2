684bc2265d546c9361b52a104ea1015af62cdf06
BalinValery <valera.balin00@mail.ru>
2025-11-11 12:33:41 +0500

    Add project files.

---
+++ Tyuiu.BalinVV.Sprint2.Task0.V29.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BalinVV.Sprint2.Task0.V29.Lib;
+namespace Tyuiu.BalinVV.Sprint2.Task0.V29.Test
+
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCompareOperations()
+        {
+            DataService ds = new DataService();
+            int x = 105;
+            int y = 735;
+            bool[] wait = new bool[6] { false, false, true, true, false, false };
+            CollectionAssert.AreEqual(wait, ds.GetCompareOperations(x, y));
+        }
+    }
+}
---
+++ Tyuiu.BalinVV.Sprint2.Task0.V29/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.BalinVV.Sprint2.Task0.V29.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int x = 105;
+        int y = 735;
+        bool[] res = new bool[6];
+        res = ds.GetCompareOperations(x, y);
+
+        Console.Title = "Спринт #1 | Выполнил: Балин В.В. | СМАРТб-25-1";
+        Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+        Console.WriteLine("* Спринт #2" + String.Concat(Enumerable.Repeat(" ", 63)) + "*");
+        Console.WriteLine("* Тема: Операции сравнения                                                *");
+        Console.WriteLine("* Задание #0" + String.Concat(Enumerable.Repeat(" ", 62)) + "*");
+        Console.WriteLine("* Вариант 29" + String.Concat(Enumerable.Repeat(" ", 62)) + "*");
+        Console.WriteLine("* Выполнил: Балин В.В. | СМАРТб-25-1" + String.Concat(Enumerable.Repeat(" ", 38)) + "*");
+        Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+        Console.WriteLine("* УСЛОВИЕ:" + String.Concat(Enumerable.Repeat(" ", 64)) + "*");
+        Console.WriteLine("* Написать програму, которая сравнивает значения, и добовляет их в массив *");
+        
+        Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:" + String.Concat(Enumerable.Repeat(" ", 56)) + "*");
+        Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+
+        Console.WriteLine("x = 105, y = 735");
+
+        Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+        Console.WriteLine("* Результат:" + String.Concat(Enumerable.Repeat(" ", 62)) + "*");
+        Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+
+        for (int i = 0; i < 6; i++)
+        {
+            Console.WriteLine(res[i]);
+        }
+        Console.ReadKey();
+    }
+}
