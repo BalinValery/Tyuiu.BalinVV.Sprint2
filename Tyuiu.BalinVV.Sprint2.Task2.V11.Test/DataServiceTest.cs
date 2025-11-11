7ff05cdd6c5ead8e35f06e0e2e79450f28089861
BalinValery <valera.balin00@mail.ru>
2025-11-11 19:12:39 +0500

    Task2

---
+++ Tyuiu.BalinVV.Sprint2.Task2.V11.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.BalinVV.Sprint2.Task2.V11.Lib;
+namespace Tyuiu.BalinVV.Sprint2.Task2.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidChekDotInShadedArea()
+        {
+            DataService ds = new DataService();
+
+            int x = 12;
+            int y = 11;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(true, res);
+        }
+    }
+}
